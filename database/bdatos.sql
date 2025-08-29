5a2298f515642b5550d8a9d2aed05273e1ccceeb
miguelcruz <miguelcruz>
2025-08-28 20:49:42 -0500

    base de datos

---
+++ database/bdatos.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS tienda2;
+
+USE tienda2;
+
+CREATE TABLE IF NOT EXISTS t_usuarios(
+    id_usuario int(11)      auto_increment NOT NULL,
+    nombre     varchar(100)                NOT NULL,
+    apellido   varchar(100)                NOT NULL,
+    email      varchar(200)                NOT NULL,
+    password   varchar(200)                NOT NULL,
+    rol        varchar(20)                 NOT NULL,
+    imagen     varchar(200),               
+    CONSTRAINT pk_usuario       PRIMARY KEY(id_usuario),
+    CONSTRAINT uq_email         UNIQUE(email)
+
+)ENGINE=Innodb;
+
+INSERT INTO t_usuarios VALUE(NULL, 'miguel','cruz','miguelcruz201715@gmail.com','123456','admin','imagen');

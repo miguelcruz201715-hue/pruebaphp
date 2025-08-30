bfd454369bfecac6ccccc4ab09f90c06c2e701f6
miguelcruz <miguelcruz>
2025-08-29 20:34:14 -0500

    base de datos t_categorias t_productos

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -16,3 +16,27 @@ CREATE TABLE IF NOT EXISTS t_usuarios(
 )ENGINE=Innodb;
 
 INSERT INTO t_usuarios VALUE(NULL, 'miguel','cruz','miguelcruz201715@gmail.com','123456','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS t_categorias(
+    id_categoria      int(11) auto_increment  NOT NULL,
+    nombre_categoria    varchar(100)            NOT NULL,
+    CONSTRAINT pk_categoria     PRIMARY KEY(id_categoria)
+)ENGINE=InnoDb;
+
+INSERT INTO t_categorias VALUE(NULL, 'Construccion'),(NULL,'electricos'),(NULL,'pinturas'),(NULL,'pisos');
+
+CREATE TABLE IF NOT EXISTS t_producto(
+    id_producto         int(11)      auto_increment NOT NULL,
+    nombre_producto     varchar(100)                NOT NULL,
+    descripcion         varchar(100)                NOT NULL,
+    stock               int(100)                    NOT NULL,
+    precio              float(10,9)                 NOT NULL,
+    imagen              varchar(255)                NOT NULL,
+    oferta              varchar(255)                NOT NULL,
+    fecha               date                        NOT NULL,
+    id_categoria        int(11)                     NOT NULL,             
+    CONSTRAINT pk_producto       PRIMARY KEY(id_producto),
+    CONSTRAINT fk_producto_categoria     FOREIGN KEY(id_categoria) REFERENCES t_categorias(id_categoria)
+    
+)ENGINE=Innodb;
+
